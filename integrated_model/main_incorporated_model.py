ed2a25b8eabc0658fe8064f3387d5bb23893ed52
JoseDiasV <gambulango7@gmail.com>
2025-12-23 10:46:38 +0000

    new output files added

--- integrated_model/main_incorporated_model.py
+++ integrated_model/main_incorporated_model.py
@@ -29,15 +29,15 @@ if __name__ == "__main__":
     n_classes = 8 # should be 8 or 3 (original paper did 3)
     # higher batch sizes increase VRAM and RAM usage, but can drastically improve both 
     # running times and model conversion along epochs
-    batch_size = 64
-    n_fold_CV = 3 # should be 10 or 3 (original paper did 3)
-    n_epochs_CNN = 10 # should be 10 (original paper had 10)
+    batch_size = 128
+    n_fold_CV = 10 # should be 10 or 3 (original paper did 3)
+    n_epochs_CNN = 20 # should be 10 (original paper had 10)
     n_epochs_softmax = 10 # should be 10
-    n_epochs_lstm = 10 # should be 10 or 9 (original paper had 9)
+    n_epochs_lstm = 20 # should be 10 or 9 (original paper had 9)
     rf_trees = 500 # should be 500 (original paper had 500)
     # weight_CNNs + weight_LSTM_rf = 1.0
-    weight_CNNs = 0.3 # probability weight value for CNN-S (can be either 0.5, 0.3, 0.4 or 0.7)
-    weight_LSTM_rf = 0.7 # probability weight value for LSTM-RF (can be either 0.5, 0.7, 0.6 or 0.3)
+    weight_CNNs = 0.5 # probability weight value for CNN-S (can be either 0.5, 0.3, 0.4 or 0.7)
+    weight_LSTM_rf = 0.5 # probability weight value for LSTM-RF (can be either 0.5, 0.7, 0.6 or 0.3)
     # dataset fraction taken with the bash command:
     # $ awk -v max=$(($(grep -c '^>' cullatraldata.txt)/5)) '/^>/{c++} c<=max' cullatraldata.txt > cullatraldata_one_fifth.txt
     path_db = "cullatraldata_one_fifth.txt" # one fifth of the original dataset, drastically helps with memory usage and running times
